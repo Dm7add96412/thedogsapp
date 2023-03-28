a20b8ddbb91803b10df2fe5165c47db8167262d2
Dm7add9 <juha.haipus@gmail.com>
2023-03-28 21:16:28 +0300

    initial commit dog app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useState,useEffect} from "react"
+import axios from "axios"
+import './App.css'
+
+export interface Breed {
+  Dog: {
+    id: string,
+    name: string,
+    url: string
+  }[]
+}
 
 function App() {
+  const [dogs, setDogs] = useState<Breed['Dog']>([])
+  const [show, showDogs] = useState<Boolean>(false)
+  const [breed, setBreed] = useState<string | null>(null)
+  const [image, setImage] = useState<string | null>(null)
+
+  const handleButtonClick = () => {
+    showDogs(true)
+  }
+
+  useEffect(() => {
+    axios
+      .get(`https://dog.ceo/api/breeds/list/all`)
+      .then(response => {
+        const dogsList: Breed['Dog'] = Object.keys(response.data.message).map((breedId) => {
+          const breedName = breedId.charAt(0).toUpperCase() + breedId.substring(1)
+          return {
+            id: breedId,
+            name: breedName,
+            url: `https://dog.ceo/api/breed/${breedId}/images`
+          }
+        })
+        setDogs(dogsList)
+      }) 
+    }, [])
+
+  const handleClick = async (name: string, url: string) => {
+    setBreed(name)
+    console.log(name)
+    const respone = await fetch(url)
+    const data = await respone.json()
+    const pic = data.message[Math.floor(Math.random() * data.message.length)]
+    setImage(pic)
+    console.log(pic)
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Top">
+        <h1>THE DOG APP</h1>
+          <button type="button" onClick={handleButtonClick}>Show me the DOGS!</button><br/>
+      </div>
+      <div className="Bottom">
+        <div className="Doglist">
+          <ul className="list">
+          {show ? 
+            dogs.map((dog) => (
+                <li key={dog.id} onClick={() => handleClick(dog.name, dog.url)}>{dog.name}</li>
+               )) : null} 
+          </ul>
+        </div>
+        <div className="Info">
+          {image ? 
+          <img src={image} alt='dog' height='400'/> : null}
+          {breed ? <h2>{breed}</h2> : null}
+        </div>
+      </div>
+
     </div>
-  );
+  )
 }
 
 export default App;
